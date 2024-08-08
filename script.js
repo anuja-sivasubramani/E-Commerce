b5ee174ba1af242644108e91fe264cd2cc68f868
anuja-sivasubramani <161808196+anuja-sivasubramani@users.noreply.github.com>
2024-08-08 20:08:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+var sidenav=document.querySelector(".s-nav")
+function show()
+{
+    sidenav.style.left="0"
+}
+function closenav()
+{
+    sidenav.style.left="-60%"
+}
+    
+function shop()
+{
+    location.href="Collections.html";
+}
